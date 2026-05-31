e3c0710eb38d695e65a40d9343e46125c0efd51d
Raditya Ahmad <raditiaahmad50@gmail.com>
2026-05-31 21:47:02 +0700

    feat: logo cholestify

--- app.py
+++ app.py
@@ -2,10 +2,15 @@ import streamlit as st
 
 st.set_page_config(
     page_title="Cholestify App",
-    page_icon="🫀",
+    page_icon="assets/logo.png",
     layout="wide",
     initial_sidebar_state="expanded"
 )
+
+# Menambahkan logo di bagian atas sidebar
+import os
+if os.path.exists("assets/logo.png"):
+    st.logo("assets/logo.png")
 
 # Konfigurasi halaman menggunakan st.navigation (Tersedia mulai Streamlit 1.36)
 pg = st.navigation([
